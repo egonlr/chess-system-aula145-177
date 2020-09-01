bdc252e1aacd20de391908bc5077a8e4447c850e
egonlr <egonlr@hotmail.com>
2020-09-01 16:41:21 -0300

    Moving pieces

--- src/application/UI.java
+++ src/application/UI.java
@@ -37,10 +37,10 @@ public class UI {
 	
 	public static ChessPosition readChessPosition(Scanner sc) {
 		try {
-		String s = sc.nextLine();
-		char column = s.charAt(0);
-		int row = Integer.parseInt(s.substring(1));
-		return new ChessPosition(column, row);
+			String s = sc.nextLine();
+			char column = s.charAt(0);
+			int row = Integer.parseInt(s.substring(1));
+			return new ChessPosition(column, row);
 		}
 		catch (RuntimeException e) {
 			throw new InputMismatchException("Error instantiating ChessPosition. Valid vaues are form a1 to h8");
